edb3815b66dc1cf8e00f8f7d3695062c5e06d554
czklove <czklove@126.com>
2018-12-07 12:28:33 +0800

    branch20181207

---
+++ IdentityServer/Command/ApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Command
+{
+    //identityserver 需要在startup 注入的对象
+    public class ApiConfig
+    {
+        /// <summary>
+        /// 定义apiresource
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[] { new ApiResource("api", "My API") };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId="client",
+                    ClientSecrets=new []{ new Secret("secret".Sha256())},
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,//授权模式 这里采用客户端认证模式,只要clientId
+                    AllowedScopes=new[]{ "api" }//定义客户端上可访问的api组，不定义就是访问全部
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //注入IdenttityService服务
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(Command.ApiConfig.GetClients())
+                .AddInMemoryApiResources(Command.ApiConfig.GetApiResources());
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseIdentityServer();
+            app.UseHttpsRedirection();
+            //注入路由
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
